f63543441b318c98ba2f1419018a6a6cdf3152f3
Владимир <105603371+DoctorLivchic@users.noreply.github.com>
2023-05-19 20:06:39 +0500

    first commit

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# is_exhibition_space_back
---
+++ lingua_idiom/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+//import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { useState, useEffect } from "react";
+// import { supabase } from "./supabaseClient";
+// import { RequireAuth } from "./RequireAuth";
+// import { AuthProvider } from "./AuthProvider";
+
+
+
+export default function App() {
+  
+const App = ()=> {
+  return(
+    <header> Шапка</header>
+
+  )
+}
+return App
+}
+
+
