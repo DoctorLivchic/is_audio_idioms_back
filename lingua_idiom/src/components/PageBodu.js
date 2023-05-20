10ddf49fcb63f60719aaf8d40d71b86e23779598
kostyalm <113437523+kostyalm@users.noreply.github.com>
2023-05-20 13:10:45 +0500

    боду ебаный

--- lingua_idiom/src/components/PageBodu.js
+++ lingua_idiom/src/components/PageBodu.js
@@ -1,33 +1,36 @@
 import React from "react";
-import { Layout} from 'antd';
+import { Layout } from "antd";
 import page_body from "../img/page_body_right.png";
-import Ellipse from "../img/Ellipse.png"
-const {  Content } = Layout;
+import Ellipse from "../img/Ellipse.png";
+const { Content } = Layout;
 
 // npm i antd что бы работало с компонентами
-class PageBodu extends React.Component{   
-    render(){      
-        return(
-            <div  >
-            <Content className="Contentleft">
-           <h1 className="page_body_p1">Откройте мир фразеологизмов!</h1>
-           <h4 className="page_body_p2">Мы предлагаем платформу для тех, кто занимаеться изучением французского или корейского языков и столкнулся с проблемой перевода фразеологизмов и идиом</h4>
+class PageBodu extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="Contentleft">
+          <h1 className="page_body_p1">Откройте мир фразеологизмов!</h1>
+          <h4 className="page_body_p2">
+            Мы предлагаем платформу для тех, кто занимаеться изучением
+            французского или корейского языков и столкнулся с проблемой перевода
+            фразеологизмов и идиом
+          </h4>
           <div className="page_button ">
             <p className="page_button_text">Начать переводить</p>
-            <div className="page_button_circle" >
-                <div className="page_button_circl1"></div>
-                <img className="" src={Ellipse} />
-                
-            </div>     
+            <div className="page_button_circle">
+              <div className="page_button_circl1"></div>
+              <img className="" src={Ellipse} />
+            </div>
           </div>
-           </Content>
-           <Content className="Contentright">
-           <div >
-           <img  src={page_body} />
-           </div>
-           </Content>
-           </div>
-        )
-    }
+        </div>
+        <div className="Contentright">
+          <div>
+            <img src={page_body} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-export default PageBodu
+export default PageBodu;
