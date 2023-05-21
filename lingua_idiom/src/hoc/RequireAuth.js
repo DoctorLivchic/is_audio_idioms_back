91fc603562601f40fb213e23b613099379705749
kostyalm <113437523+kostyalm@users.noreply.github.com>
2023-05-21 15:19:30 +0500

    починил auth

--- lingua_idiom/src/hoc/RequireAuth.js
+++ lingua_idiom/src/hoc/RequireAuth.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import { useLocation, Navigate } from "react-router-dom";
-import useAuth from "./useAuth";
+import { useAuth } from "./useAuth";
 
 const RequireAuth = (children) => {
   const location = useLocation();
   const { user } = useAuth();
 
-  if (!auth) {
+  if (!user) {
     return <Navigate to="/pages/loginpage" state={{ from: location }} />;
   }
 
