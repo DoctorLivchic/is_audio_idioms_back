e3ee975d7813f19f72b1c116bbc267a4f14fb40a
kostyalm <113437523+kostyalm@users.noreply.github.com>
2023-06-03 16:55:09 +0500

    первая версия избранного

--- lingua_idiom/src/components/FavoritesBody.js
+++ lingua_idiom/src/components/FavoritesBody.js
@@ -4,14 +4,14 @@ import { useLocation, useNavigate } from "react-router-dom";
 import User from "../img/User.png";
 import { useEffect, useState } from "react";
 import profileDiv from "../img/profileDiv.png";
-import { Table, notification,Button, Form, Input, Select , Modal} from 'antd';
+import { Table, notification, Button, Form, Input, Select, Modal } from "antd";
 import { useForm } from "antd/es/form/Form";
 import { supabase } from "../supabase/supabaseClient";
 
 function FavoritesBody() {
   const navigate = useNavigate();
   const { signout } = useAuth();
-
+  const [phraseIds, setPhraseIds] = useState([]);
   const [phrase_text, setrequest] = useState([]);
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
   const [show, setShow] = useState(false);
@@ -65,39 +65,78 @@ function FavoritesBody() {
     onChange: onSelectChange,
   };
 
+  // useEffect(() => {
+  //   getphrase().then(() => setLoading(false));
+  // }, [loading]);
+
   useEffect(() => {
-    getphrase().then(() => setLoading(false));
-  }, [loading]);
+    const fetchUserIds = async () => {
+      const { data, error } = await supabase
+        .from("favourites_phraseological")
+        .select("phrase_id, language_id")
+        .eq("user_id", localStorage.getItem("userID"))
 
+      if (error) console.log("Error fetching user IDs:", error.message);
+      else {
+        setPhraseIds(data.map((phr_id) => phr_id.phrase_id));
+      }
+    };
 
-  async function getphrase() {
-    const phraseological = await supabase.from("favourites_phraseological").select();
-    const phre = (await phraseological).data;
-    const data = await supabase
-      .from("favourites_phraseological")
-      .select()
-      .order("phrase_text_id");
+    fetchUserIds();
+  }, []);
 
-  }
+  useEffect(() => {
+    const fetchOrders = async () => {
+      const { data, error } = await supabase
+        .from("phrase_text")
+        .select(
+          "phrase_text_text, phrase_text_transcription, phrase_text_desc, phrase_id "
+        )
+        .in("phrase_id", phraseIds);
 
+      console.log(data);
+    };
+
+    if (phraseIds.length > 0) fetchOrders();
+  }, [phraseIds]);
+  // async function getphrase() {
+  //   try {
+  //     const phraseId = await supabase
+  //       .from("favourites_phraseological")
+  //       .select("phrase_id")
+  //       .eq("user_id", localStorage.getItem("userID"));
+
+  //     const arr = [];
+  //     for (let i = 0; i < phraseId.length; i++) {
+  //       arr[i] = phraseId[i]["phrase_id"];
+  //     }
+
+  //     const phrase = await supabase
+  //       .from("phrase_text")
+  //       .select(
+  //         "phrase_text_text, phrase_text_transcription, phrase_text_desc, phrase_id "
+  //       )
+  //       .in("phrase_id", arr);
+
+  //     console.log(phraseId);
+  //     console.log(arr);
+  //   } catch (error) {
+  //     console.log(error.message);
+  //   }
+  // }
 
   return (
     <div className="FavoritesBody">
-    
-    <Table
+      <Table
         loading={loading}
         dataSource={phrase_text}
         columns={columns}
         rowSelection={rowSelection}
         rowKey={(record) => record.phrase_text_id}
         onRow={(record) => ({
-          onClick: () => {
-          
-          },
+          onClick: () => {},
         })}
       />
-      
-    
     </div>
   );
 }
